f0213e030c71214a84c09c1fd9e84a6d59a29cf1
Maryam Yazdanparast <Yazdanparast.Maryam@gmail.com>
2025-02-17 14:58:24 +0000

    wip

--- R_Complement_Transcript.py
+++ R_Complement_Transcript.py
@@ -1,41 +1,37 @@
 def enq():
     return input("what is your sequence type (d for DNA - r for RNA)")
 
-
 def input_seq(strand):
     return input("Enter your sequence: ")
 
-
 def enquiry_type():
     return input(
-        "what is your enquiry (c for complement - t for transcript - r for reverse - n for reverse complement)"
-    )
-
-
+        "what is your enquiry (c for complement - t for transcript - r for reverse - n for reverse complement)")
 # reversed complement of a strand equals to its negative strand hence n was selected.
 
-if enquiry_type() in ["c", "n", "t"]:
+def results():
+    complement_transcript = ""
+    codon_dict: dict[str, str] = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C",
+        "a": "t",
+        "t": "a",
+        "c": "g",
+        "g": "c",
+    }
+    if enquiry_type() in ["c", "n", "t"]:
+        def complement_seq(strand):
+            base_code = {"A", "T", "G", "C", "U", "a", "t", "g", "c", "u"}
+            for single_base in strand:
+                if single_base not in base_code:
+                    return "Error: Input must include letters a, t/u, g, c only."
+                if (enq() == "d") and ("U" or "u" in strand):
+                    return "Error: DNA strand cannot contain U/u."
+                if (enq() == "r") and ("T" or "t" in strand):
+                    return "Error: RNA strand cannot contain T/t."
 
-    def complement_seq(strand):
-        base_code = {"A", "T", "G", "C", "U", "a", "t", "g", "c", "u"}
-        for single_base in strand:
-            if single_base not in base_code:
-                return "Error: Input must include letters a, t/u, g, c only."
-            if (enq() == "d") and ("U" or "u" in strand):
-                return "Error: DNA strand cannot contain U/u."
-            if (enq() == "r") and ("T" or "t" in strand):
-                return "Error: RNA strand cannot contain T/t."
-        complement_transcript = ""
-        codon_dict: dict[str, str] = {
-            "A": "T",
-            "T": "A",
-            "C": "G",
-            "G": "C",
-            "a": "t",
-            "t": "a",
-            "c": "g",
-            "g": "c",
-        }
 
     if input == "r":
         complement_codon["U"] = "A"
