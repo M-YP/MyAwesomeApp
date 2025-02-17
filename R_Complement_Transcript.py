0007c68630b75c9b143863b0b50c6421a679cf90
Maryam Yazdanparast <Yazdanparast.Maryam@gmail.com>
2025-02-17 14:50:54 +0000

    wip

--- R_Complement_Transcript.py
+++ R_Complement_Transcript.py
@@ -1,4 +1,4 @@
-def seq_type():
+def enq():
     return input("what is your sequence type (d for DNA - r for RNA)")
 
 
@@ -13,29 +13,29 @@ def enquiry_type():
 
 
 # reversed complement of a strand equals to its negative strand hence n was selected.
-if enquiry_type() == "n" or "t" or "n":
-    complement_transcript = ""
-    codon_dict: dict[str, str] = {
-        "A": "T",
-        "T": "A",
-        "C": "G",
-        "G": "C",
-        "a": "t",
-        "t": "a",
-        "c": "g",
-        "g": "c",
-    }
 
+if enquiry_type() in ["c", "n", "t"]:
 
-def complement_seq(strand):
-    base_code = {"A", "T", "G", "C", "U", "a", "t", "g", "c", "u"}
-    for codon in strand:
-        if codon not in base_code:
-            return "Error: Input must include letters a, t/u, g, c only."
-        if (seq_type() == "d") and ("U" or "u" in strand):
-            return "Error: DNA strand cannot contain U/u."
-        if (seq_type() == "r") and ("T" or "t" in strand):
-            return "Error: RNA strand cannot contain T/t."
+    def complement_seq(strand):
+        base_code = {"A", "T", "G", "C", "U", "a", "t", "g", "c", "u"}
+        for single_base in strand:
+            if single_base not in base_code:
+                return "Error: Input must include letters a, t/u, g, c only."
+            if (enq() == "d") and ("U" or "u" in strand):
+                return "Error: DNA strand cannot contain U/u."
+            if (enq() == "r") and ("T" or "t" in strand):
+                return "Error: RNA strand cannot contain T/t."
+        complement_transcript = ""
+        codon_dict: dict[str, str] = {
+            "A": "T",
+            "T": "A",
+            "C": "G",
+            "G": "C",
+            "a": "t",
+            "t": "a",
+            "c": "g",
+            "g": "c",
+        }
 
     if input == "r":
         complement_codon["U"] = "A"
@@ -43,8 +43,8 @@ def complement_seq(strand):
         complement_codon["A"] = "U"
         complement_codon["a"] = "u"
 
-    for codon in strand:
-        complement += complement_codon[codon]
+    for base in strand:
+        complement += complement_codon[base]
     return complement
 
 
@@ -61,6 +61,6 @@ def reversed_complement(dna_strand):
     print("your reversed cDNA strand is:", result)
 
 
-seq_type = seq_type()
+seq_type = enq()
 query_seq = input_seq()
 reversed_complement(query_seq)
